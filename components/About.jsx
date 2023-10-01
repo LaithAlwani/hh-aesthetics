3841bf99de0ffe1f7f2d6d2a789b12bbe38545f5
laith <laithalwani@gmail.com>
2023-10-01 13:39:23 -0400

    fixes build errors

--- components/About.jsx
+++ components/About.jsx
@@ -3,14 +3,14 @@ export default function About() {
     <div className="about">
       <h2>About</h2>
       <p>
-        Health Heaven Aesthetics is Ottawa’s premier laser clinic, providing aesthetic laser
+        Health Heaven Aesthetics is Ottawa&apos;s premier laser clinic, providing aesthetic laser
         treatments, for over 15 years. Our technicians are trained in the latest techniques and use
         the latest equipment to have you looking and feeling your best.
       </p>
       <p>
         At Health Haven Aesthetics Clinic, we are committed to helping you achieve your beauty and
         wellness goals. Our state-of-the-art facility offers a comprehensive range of services
-        designed to enhance your natural beauty and rejuvenate your skin. Whether you're looking for
+        designed to enhance your natural beauty and rejuvenate your skin. Whether you&apos;re looking for
         subtle enhancements with Botox and dermal fillers, advanced laser skin treatments, or
         permanent hair reduction through laser hair removal, our experienced team is here to provide
         you with personalized care and exceptional results.
